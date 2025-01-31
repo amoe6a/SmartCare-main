e0df4d3271b2b7e975eb4b19a76c8072c7a53835
amoe6a <amoeba@DESKTOP-2V4DEMO>
2025-01-31 10:02:54 +0500

    stable

---
+++ front/app.py
@@ -0,0 +1,37 @@
+import time
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+from bokeh.plotting import figure
+
+st.set_page_config(
+    page_title="Real-Time ECG readings Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
+st.title('Real-Time ECG readings from AD8232 sensor on ESP32 board')
+
+DATE_COLUMN = 'date_created'
+DATA_URL = ("https://smartcare-main-production.up.railway.app/sensors/1")
+
+# @st.cache_data
+def load_data(nrows):
+    json_data = requests.get(DATA_URL).json()
+    data = pd.DataFrame(json_data)
+    return data.tail(nrows)
+
+placeholder = st.empty()
+
+while True:
+    data = load_data(100)
+    with placeholder.container():
+        y = data["reading_value"]
+        x = list(range(y.size))
+
+        p = figure(title="ECG Readings", x_axis_label="S", y_axis_label="R")
+        p.line(x, y, legend_label="Trend", line_width=2)
+
+        st.bokeh_chart(p, use_container_width=True)
+
+    time.sleep(1)
