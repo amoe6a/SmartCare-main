13b2734f55fe72bba87580cc8c0f2fa981866fda
amoe6a <amoeba@DESKTOP-2V4DEMO>
2025-01-31 15:33:29 +0500

    updated webapp to show bpm

--- front/app.py
+++ front/app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import requests
 from bokeh.plotting import figure
+import neurokit2 as nk
+import heartpy as hp
 
 st.set_page_config(
     page_title="Real-Time ECG readings Dashboard",
@@ -14,6 +16,7 @@ st.title('Real-Time ECG readings from AD8232 sensor on ESP32 board')
 
 DATE_COLUMN = 'date_created'
 DATA_URL = ("https://smartcare-main-production.up.railway.app/sensors/1")
+SAMPLING_RATE = 100
 
 # @st.cache_data
 def load_data(nrows):
@@ -24,7 +27,28 @@ def load_data(nrows):
 placeholder = st.empty()
 
 while True:
-    data = load_data(100)
+    data = load_data(1000)
+    sd = [float(each) for each in data["reading_value"]]
+    sdf = np.array(sd).flatten()
+
+    # st.write(pd.DataFrame([float(each) for each in data["reading_value"]]))
+    # signals, info = nk.ecg_process(
+    #     pd.Series(
+    #         [float(each) for each in data["reading_value"]]
+    #     ),
+    #     sampling_rate=SAMPLING_RATE
+    # )
+    # st.write(info)
+
+    # df, info = nk.ecg_process(sd, sampling_rate=SAMPLING_RATE)
+    # st.write(info)
+    # analyze_epochs = nk.ecg_analyze({"0": df}, sampling_rate=100)
+    # st.write(analyze_epochs)
+
+    working_data, measures = hp.process(sdf, SAMPLING_RATE, report_time=True)
+    # anr = nk.ecg_analyze(data["reading_value"], sampling_rate=SAMPLING_RATE)
+    # st.write(anr)
+
     with placeholder.container():
         y = data["reading_value"]
         x = list(range(y.size))
@@ -32,6 +56,8 @@ while True:
         p = figure(title="ECG Readings", x_axis_label="S", y_axis_label="R")
         p.line(x, y, legend_label="Trend", line_width=2)
 
+        st.write('breathing rate is: %s Hz' %measures['breathingrate'])
+        st.write('heart rate is: %s BPM' %measures['bpm'])
         st.bokeh_chart(p, use_container_width=True)
 
     time.sleep(10000)
